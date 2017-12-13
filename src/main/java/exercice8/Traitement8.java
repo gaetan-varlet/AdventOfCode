b16651463ad5041b4fd01f92f17b2194f40a4e77
Gaëtan Varlet <gaetan.varlet@insee.fr>
2017-12-13 15:50:53 +0100

    ajout de l'exercice 8 terminé

---
+++ src/main/java/exercice8/Traitement8.java
@@ -0,0 +1,92 @@
+package exercice8;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import utils.ClotureListener;
+import utils.LigneListener;
+
+public class Traitement8 implements LigneListener, ClotureListener{
+
+    private int maximumTemporaire;
+
+    private int maximumFinal;
+
+
+    public int getMaximumTemporaire() {
+	return maximumTemporaire;
+    }
+
+    public int getMaximumFinal() {
+	return maximumFinal;
+    }
+
+    private Map<String, Integer> mapRegister =  new HashMap<String, Integer>();
+
+    public Map<String, Integer> getMapRegister() {
+	return mapRegister;
+    }
+
+
+    @Override
+    public void ligneLue(String ligne) {
+
+	String[] tab = ligne.split(" ");
+
+	if(mapRegister.containsKey(tab[0])==false){
+	    mapRegister.put(tab[0], 0);
+	}	
+	if(mapRegister.containsKey(tab[4])==false){
+	    mapRegister.put(tab[4], 0);
+	}
+
+	if("<".equals(tab[5])){
+	    if(mapRegister.get(tab[4])<Integer.valueOf(tab[6])){
+		ajoutOuRetrait(tab);
+	    }
+	} else if("<=".equals(tab[5])){
+	    if(mapRegister.get(tab[4])<=Integer.valueOf(tab[6])){
+		ajoutOuRetrait(tab);
+	    }
+	} else if(">".equals(tab[5])){
+	    if(mapRegister.get(tab[4])>Integer.valueOf(tab[6])){
+		ajoutOuRetrait(tab);
+	    }    
+	} else if(">=".equals(tab[5])){
+	    if(mapRegister.get(tab[4])>=Integer.valueOf(tab[6])){
+		ajoutOuRetrait(tab);
+	    }
+	} else if("==".equals(tab[5])){
+	    if(mapRegister.get(tab[4]).equals(Integer.valueOf(tab[6]))){
+		ajoutOuRetrait(tab);
+	    }
+	} else if("!=".equals(tab[5])){
+	    if(mapRegister.get(tab[4]).equals(Integer.valueOf(tab[6]))==false){
+		ajoutOuRetrait(tab);
+	    }
+	}
+
+	int temp = Collections.max(mapRegister.values());
+	if(temp>maximumTemporaire){
+	    maximumTemporaire=temp;
+	}
+
+    }
+
+    private void ajoutOuRetrait(String[] tab){
+	if("inc".equals(tab[1])){
+	    mapRegister.put(tab[0], mapRegister.get(tab[0])+Integer.valueOf(tab[2]));
+	} else if("dec".equals(tab[1])){
+	    mapRegister.put(tab[0], mapRegister.get(tab[0])-Integer.valueOf(tab[2]));
+	}
+    }
+
+    @Override
+    public void fichierFerme() {
+	Collection<Integer> liste = mapRegister.values();
+	maximumFinal = Collections.max(liste);	
+    }
+
+}
