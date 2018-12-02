0da4e4691bb36e72e5cf0935bdcabab4e5c6efba
Gaëtan Varlet <gaetanvarlet@gmail.com>
2018-12-02 10:13:15 +0100

    fin exo2 2018

--- src/main/java/traitement2018/Traitement02.java
+++ src/main/java/traitement2018/Traitement02.java
@@ -1,12 +1,100 @@
 package traitement2018;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import utils.ClotureListener;
 import utils.LigneListener;
 
-public class Traitement02 implements LigneListener {
+public class Traitement02 implements LigneListener, ClotureListener {
+	
+	private int doublon;
+	private int triplon;
+	private Map<Character, Integer> map;
+	
+	private List<char[]> liste = new ArrayList<>();
+	private int maxCarEnCommun;
+	private StringBuffer lettresEnCommun = new StringBuffer();
+
+	
+	public int getChecksum() {
+		return doublon * triplon;
+	}
+	
+	public String getLettresEnCommun() {
+		return lettresEnCommun.toString();
+	}
 
 	@Override
 	public void ligneLue(String ligne) {
-
+		/* on regarde pour chaque ligne le nombre d'occurence de chaque caractère
+		 * en le stockant dans une map.
+		 * On incrèmente ensuite doublon et triplon si la ligne contient des doublons/triplons
+		 */
+		char[] tableauDeLettres = ligne.toCharArray();
+		map = new HashMap<>();
+		for (char caractere : tableauDeLettres) {
+			if(map.get(caractere) != null) {
+				map.put(caractere, map.get(caractere) + 1);
+			} else {
+				map.put(caractere, 1);
+			}
+		}
+		Collection<Integer> collection = map.values();
+		if(collection.contains(2)) {
+			doublon++;
+		}
+		if(collection.contains(3)) {
+			triplon++;
+		}
+		
+		// ajout de toutes les lignes sous forme de tableau de char pour la partie 2
+		liste.add(ligne.toCharArray());
+	}
+	
+	@Override
+	public void fichierFerme() {
+		char[] tab1 = null, tab2 = null;
+		
+		// comparaison de tous les tableaux stockées dans la liste 2 à 2
+		for (int i = 0; i < liste.size(); i++) {
+			for (int j = i+1; j < liste.size(); j++) {
+				int nombreDeCaractereEnCommun = nombreDeCaractereEnCommun(liste.get(i), liste.get(j));
+				// si le nombre de caractères en commun est le plus grand rencontré jusque là
+				// on stocke ce nombre et les 2 tableaux
+				if(nombreDeCaractereEnCommun >= maxCarEnCommun) {
+					maxCarEnCommun = nombreDeCaractereEnCommun;
+					tab1 = liste.get(i);
+					tab2 = liste.get(j);
+				}
+			}
+		}
+		
+		// on stocke les lettres en commun entre les 2 tableaux dans un string
+		for (int i = 0; i < tab1.length; i++) {
+			if(tab1[i] == tab2[i]) {
+				lettresEnCommun.append(tab1[i]);
+			}
+		}
+	}
+	
+	/**
+	 * méthode qui renvoie le nombre de caractères identiques à la même position dans les tableaux de char
+	 * @param tab1
+	 * @param tab2
+	 * @return
+	 */
+	private int nombreDeCaractereEnCommun(char[] tab1, char[] tab2) {
+		int nb = 0;
+		for (int i = 0; i < tab1.length; i++) {
+			if(tab1[i] == tab2[i]) {
+				nb++;
+			}
+		}
+		return nb;
 	}
 
 }
--- src/test/java/traitement2018/Exercice02Test.java
+++ src/test/java/traitement2018/Exercice02Test.java
@@ -1,15 +1,51 @@
 package traitement2018;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import org.junit.Test;
 
+import utils.MonReader;
+
 public class Exercice02Test{
 
+	private static Charset charset = StandardCharsets.UTF_8;
+
 	@Test
-	public void traitementTest1() throws IOException{
+	public void traitementTest1() throws IOException{	
+		MonReader monReader = new MonReader();
+		Traitement02 traitement = new Traitement02();
+		monReader.addLigneListener(traitement);
+		monReader.setClotureListener(traitement);
+		monReader.lireFichier("src/test/resources/traitement2018/inputExercice02Test1.txt", charset);
+
+		assertEquals(12, traitement.getChecksum());
 	}
 	
+	@Test
+	public void traitementTest2() throws IOException{	
+		MonReader monReader = new MonReader();
+		Traitement02 traitement = new Traitement02();
+		monReader.addLigneListener(traitement);
+		monReader.setClotureListener(traitement);
+		monReader.lireFichier("src/test/resources/traitement2018/inputExercice02Test2.txt", charset);
 
+		assertEquals("fgij", traitement.getLettresEnCommun());
+	}
+	
+	@Test
+	public void traitement() throws IOException{	
+		MonReader monReader = new MonReader();
+		Traitement02 traitement = new Traitement02();
+		monReader.addLigneListener(traitement);
+		monReader.setClotureListener(traitement);
+		monReader.lireFichier("src/test/resources/traitement2018/inputExercice02.txt", charset);
+
+		assertEquals(7776, traitement.getChecksum());
+		assertEquals("wlkigsqyfecjqqmnxaktdrhbz", traitement.getLettresEnCommun());
+	}
 
 }
