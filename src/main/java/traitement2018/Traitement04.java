2ba0eaae5c5519f5679129f45d62d1cf8031c035
Gaëtan Varlet <gaetanvarlet@gmail.com>
2018-12-05 22:18:01 +0100

    début exo4 20218

--- src/main/java/traitement2018/Traitement04.java
+++ src/main/java/traitement2018/Traitement04.java
@@ -3,10 +3,11 @@ package traitement2018;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import utils.ClotureListener;
 import utils.LigneListener;
-
 public class Traitement04 implements LigneListener, ClotureListener {
 	
 	private int resultat;
@@ -24,6 +25,20 @@ public class Traitement04 implements LigneListener, ClotureListener {
 	@Override
 	public void fichierFerme() {
 		Collections.sort(listeDonnees);
+		Pattern patternLigne = Pattern.compile("\\[(....-..-.. ..:..)\\] (.*)");
+		Pattern patternTexte = Pattern.compile("Guard #(.*) begins shift");
+		for (String ligne : listeDonnees) {
+			Matcher matcherLigne = patternLigne.matcher(ligne);
+			matcherLigne.matches();
+			String dateTime = matcherLigne.group(1);
+			String texte = matcherLigne.group(2);
+			if(texte.contains("Guard")) {
+				Matcher matcherTexte = patternTexte.matcher(texte);
+				matcherTexte.matches();
+				int idGuard = Integer.parseInt(matcherTexte.group(1));
+				System.out.println();
+			}
+		}
 		System.out.println(listeDonnees);
 	}
 
