2ea276a50c8c79b31cad562e87f8d17658842486
Gaëtan Varlet <gaetan.varlet@insee.fr>
2018-12-05 11:31:52 +0100

    fin exo5 2018

---
+++ src/main/java/traitement2018/Traitement05.java
@@ -0,0 +1,82 @@
+package traitement2018;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import utils.LigneListener;
+
+public class Traitement05 implements LigneListener {
+	
+	private int nombreLettresRestantesDansLaFormule;
+	private int nombreLettresRestantesDansLaFormuleSimplifiee;
+	
+	private Map<Character, Integer> mapLettresTailleFormule = new HashMap<>();
+	
+	public int getNombreLettresRestantesDansLaFormule() {
+		return nombreLettresRestantesDansLaFormule;
+	}
+	
+	public int getNombreLettresRestantesDansLaFormuleSimplifiee() {
+		return nombreLettresRestantesDansLaFormuleSimplifiee;
+	}
+	
+	@Override
+	public void ligneLue(String ligne) {
+		
+		char[] tableauChar = ligne.toCharArray();
+		List<Character> liste = new ArrayList<>();
+		Set<Character> ensembleDesLettres = new HashSet<>();
+		for (char character : tableauChar) {
+			liste.add(character);
+			ensembleDesLettres.add(Character.toUpperCase(character));
+		}
+		
+		nombreLettresRestantesDansLaFormule = calculLongueurFormule(liste);
+		
+		for (Character character : ensembleDesLettres) {
+			List<Character> listeTronque = new ArrayList<>();
+			listeTronque.addAll(liste);
+			Set<Character> ensembleCaractereCourant = new HashSet<>();
+			ensembleCaractereCourant.add(character);
+			ensembleCaractereCourant.add(Character.toLowerCase(character));
+			listeTronque.removeAll(ensembleCaractereCourant);
+			mapLettresTailleFormule.put(character, calculLongueurFormule(listeTronque));
+		}
+		
+		nombreLettresRestantesDansLaFormuleSimplifiee = nombreLettresRestantesDansLaFormule;
+		for (Integer longueurFormule : mapLettresTailleFormule.values()) {
+			if(longueurFormule<nombreLettresRestantesDansLaFormuleSimplifiee) {
+				nombreLettresRestantesDansLaFormuleSimplifiee = longueurFormule;
+			}
+		}
+		
+		
+	}
+	
+	private int calculLongueurFormule(List<Character> listeCaractere) {
+		List<Character> liste = new ArrayList<>();
+		liste.addAll(listeCaractere);
+		boolean fin = false;
+		while(!fin) {
+			int tailleListe = liste.size();
+			for (int i = 0 ; i < liste.size() -1 ; i++) {
+				if((Character.toUpperCase(liste.get(i)) == Character.toUpperCase(liste.get(i+1))) && (liste.get(i) != liste.get(i+1))) {
+					//System.out.println(i +" "+ liste.get(i) +" "+ liste.get(i+1));
+					liste.remove(i+1);
+					liste.remove(i);
+					//System.out.println(liste);
+					break;
+				}		
+			}
+			if(liste.size() == tailleListe) {
+				fin = true;
+			}
+		}		
+		return liste.size();
+	}
+	
+}
---
+++ src/test/java/traitement2018/Exercice05Test.java
@@ -0,0 +1,42 @@
+package traitement2018;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.Test;
+
+import utils.MonReader;
+
+public class Exercice05Test{
+
+	private static Charset charset = StandardCharsets.UTF_8;
+
+	@Test
+	public void traitementTest1() throws IOException{	
+		MonReader monReader = new MonReader();
+		Traitement05 traitement = new Traitement05();
+		monReader.addLigneListener(traitement);
+		monReader.lireFichier("src/test/resources/traitement2018/inputExercice05Test1.txt", charset);
+
+		assertEquals(10, traitement.getNombreLettresRestantesDansLaFormule());
+		assertEquals(4, traitement.getNombreLettresRestantesDansLaFormuleSimplifiee());
+	}
+	
+	@Test
+	public void traitement() throws IOException{	
+		MonReader monReader = new MonReader();
+		Traitement05 traitement = new Traitement05();
+		monReader.addLigneListener(traitement);
+		monReader.lireFichier("src/test/resources/traitement2018/inputExercice05.txt", charset);
+
+		assertEquals(11894, traitement.getNombreLettresRestantesDansLaFormule());
+		assertEquals(5310, traitement.getNombreLettresRestantesDansLaFormuleSimplifiee());
+	}
+
+	
+
+
+}
