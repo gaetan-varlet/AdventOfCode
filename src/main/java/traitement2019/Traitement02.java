c26b26b4d27975d4d56a3d384f9c5b93c9edc208
Gaëtan Varlet <gaetanvarlet@gmail.com>
2019-12-01 21:50:59 +0100

    prepa exo2 2019

---
+++ src/main/java/traitement2019/Traitement02.java
@@ -0,0 +1,11 @@
+package traitement2019;
+
+import utils.LigneListener;
+
+public class Traitement02 implements LigneListener {
+
+	@Override
+	public void ligneLue(String ligne) {
+	}
+
+}
---
+++ src/test/java/traitement2019/Exercice02Test.java
@@ -0,0 +1,29 @@
+package traitement2019;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import org.junit.Test;
+import utils.MonReader;
+
+public class Exercice02Test {
+
+	private static Charset charset = StandardCharsets.UTF_8;
+
+	@Test
+	public void traitementTest1() throws IOException {
+		MonReader monReader = new MonReader();
+		Traitement02 traitement2 = new Traitement02();
+		monReader.addLigneListener(traitement2);
+		monReader.lireFichier("src/test/resources/traitement2019/inputExercice02Test1.txt", charset);
+	}
+
+	@Test
+	public void traitement() throws IOException {
+		MonReader monReader = new MonReader();
+		Traitement02 traitement2 = new Traitement02();
+		monReader.addLigneListener(traitement2);
+		monReader.lireFichier("src/test/resources/traitement2019/inputExercice02.txt", charset);
+	}
+
+}
