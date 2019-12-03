82a59b789dd7168269dc2c641a8ff0da051f3a89
Gaëtan Varlet <gaetanvarlet@gmail.com>
2019-12-03 20:16:36 +0100

    refactor tests avec beforeEach

--- src/test/java/traitement2019/Exercice02Test.java
+++ src/test/java/traitement2019/Exercice02Test.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import utils.MonReader;
@@ -14,69 +15,50 @@ public class Exercice02Test {
 
 	private static Charset charset = StandardCharsets.UTF_8;
 
-	@Test
-	public void traitementTest1() throws IOException {
-		MonReader monReader = new MonReader();
-		Traitement02 traitement2 = new Traitement02();
+	MonReader monReader;
+	Traitement02 traitement2;
+
+	@BeforeEach
+	public void init() {
+		monReader = new MonReader();
+		traitement2 = new Traitement02();
 		monReader.addLigneListener(traitement2);
 		monReader.setClotureListener(traitement2);
-		monReader.lireFichier("src/test/resources/traitement2019/inputExercice02Test1.txt", charset);
+	}
 
+	@Test
+	public void traitementTest1() throws IOException {
+		monReader.lireFichier("src/test/resources/traitement2019/inputExercice02Test1.txt", charset);
 		assertEquals(3500, traitement2.getResultat());
 	}
 
 	@Test
 	public void traitementTest2() throws IOException {
-		MonReader monReader = new MonReader();
-		Traitement02 traitement2 = new Traitement02();
-		monReader.addLigneListener(traitement2);
-		monReader.setClotureListener(traitement2);
 		monReader.lireFichier("src/test/resources/traitement2019/inputExercice02Test2.txt", charset);
-
 		assertEquals(2, traitement2.getResultat());
 	}
 
 	@Test
 	public void traitementTest3() throws IOException {
-		MonReader monReader = new MonReader();
-		Traitement02 traitement2 = new Traitement02();
-		monReader.addLigneListener(traitement2);
-		monReader.setClotureListener(traitement2);
 		monReader.lireFichier("src/test/resources/traitement2019/inputExercice02Test3.txt", charset);
-
 		assertEquals(2, traitement2.getResultat());
 	}
 
 	@Test
 	public void traitementTest4() throws IOException {
-		MonReader monReader = new MonReader();
-		Traitement02 traitement2 = new Traitement02();
-		monReader.addLigneListener(traitement2);
-		monReader.setClotureListener(traitement2);
 		monReader.lireFichier("src/test/resources/traitement2019/inputExercice02Test4.txt", charset);
-
 		assertEquals(2, traitement2.getResultat());
 	}
 
 	@Test
 	public void traitementTest5() throws IOException {
-		MonReader monReader = new MonReader();
-		Traitement02 traitement2 = new Traitement02();
-		monReader.addLigneListener(traitement2);
-		monReader.setClotureListener(traitement2);
 		monReader.lireFichier("src/test/resources/traitement2019/inputExercice02Test5.txt", charset);
-
 		assertEquals(30, traitement2.getResultat());
 	}
 
 	@Test
 	public void traitement() throws IOException {
-		MonReader monReader = new MonReader();
-		Traitement02 traitement2 = new Traitement02();
-		monReader.addLigneListener(traitement2);
-		monReader.setClotureListener(traitement2);
 		monReader.lireFichier("src/test/resources/traitement2019/inputExercice02.txt", charset);
-
 		assertEquals(4023471, traitement2.getResultat());
 		assertEquals(8051, traitement2.getResultat2());
 	}
